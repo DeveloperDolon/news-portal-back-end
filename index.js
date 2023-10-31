9f2c541c97cadf323e963964e6c7004c6052b22b
Dolon Roy <dolonr718@gmail.com>
2023-10-31 21:37:32 +0600

    send all news data from database commit

--- index.js
+++ index.js
@@ -1,2 +1,75 @@
-console.log("hello world!");
-console.log("hello from backend ");
+require("dotenv").config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DATA_USERNAME}:${process.env.DATA_PASSWORD}@cluster0.evacz3b.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection]
+
+    const allNewsCollection = client.db("planetNewsDB").collection("allNewsCollection");
+
+    app.get("/all-news", async (req, res) => {
+
+        try{
+
+            const options = {
+                projection: {
+                    headline : 1,
+                    image: 1,
+                    date_published: 1,
+                    author: 1
+                }
+            }
+
+            const cursor = allNewsCollection.find({},options);
+            const news = await cursor.toArray();
+
+            res.send(news);
+        }catch(error) {
+            console.log(error);
+        }
+    })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.send("Welcome to Planet News Server (❁´◡`❁)");
+})
+
+app.listen(port, () => {
+    console.log("Server listening on port", port);
+}) 
