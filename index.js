174a37c045cd6078e71461d57c1f4c6e5754e465
Dolon Roy <dolonr718@gmail.com>
2023-11-01 20:35:28 +0600

    single news data loading from database commit

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+var jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
+app.use(cookieParser());
 
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DATA_USERNAME}:${process.env.DATA_PASSWORD}@cluster0.evacz3b.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,6 +28,30 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+const logger = (req, res, next) => {
+  console.log("log info : ", req.method, req.url);
+  next();
+} 
+
+const verify = (req, res, next) => {
+  const token = req?.cookies?.token;
+
+  if(!token) {
+    return res.status(401).send({message: "unauthorized access"}); 
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decode) => {
+    if(err) {
+      return res.status(500).send({message: "unauthorized access"});
+    }
+
+    req.user = decode;
+    next();
+  })
+
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -30,7 +60,29 @@ async function run() {
 
     const allNewsCollection = client.db("planetNewsDB").collection("allNewsCollection");
 
-    app.get("/all-news", async (req, res) => {
+    // jwt related requests
+    app.post("/jwt", logger, async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "2h"});
+
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none"
+      }).send({success : true})
+    })
+
+    // news related requests
+
+    app.get("/news/:id", logger, async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+
+      const result = await allNewsCollection.findOne(filter);
+      res.send(result);
+    })
+
+    app.get("/all-news", logger, async (req, res) => {
 
         try{
 
@@ -50,6 +102,10 @@ async function run() {
         }catch(error) {
             console.log(error);
         }
+    })
+
+    app.get("/news/:id" , async (req, res) => {
+
     })
 
 
